67d4f7b1992de6470eac679549cbadc902786e8b
Taylor Nodell <tnodell@ssbbartgroup.com>
2017-08-24 13:50:49 -0700

    Finally passes fishtype json correctly

--- server.js
+++ server.js
@@ -3,20 +3,21 @@ const app = express();
 const fs = require('fs');
 const requireGlob = require('require-glob');
 /* require all the SOLD json files */
-const fishTypes = requireGlob.sync('./data/*SOLD.json')
-
+const fishTypes = requireGlob.sync('./data/sold/*.json')
+console.log(fishTypes.fwanabantoid);
 const port = 3000
 
-const handlebars = require('express-handlebars').create({defaultLayout:'main'});
- 
-app.engine('handlebars', handlebars.engine);
- 
+//const handlebars = require('express-handlebars').create({defaultLayout:'main'});
+
+//app.engine('handlebars', handlebars.engine);
+var exphbs = require('express-handlebars'); 
+app.engine('handlebars', exphbs({	helpers: { json: function (context) { return JSON.stringify(context); } } }));
 app.set('view engine', 'handlebars');
 
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', (request, response) => {  
-  response.render('home', fishTypes)
+app.get('/', (request, response) => { 
+  response.render('home', { fishTypes: fishTypes });
 })
 
 app.listen(port, (err) => {  
